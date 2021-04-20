8101012a838a0c225acf95860ac483082e20949e
Pankaj Sharma <panksh249@gmail.com>
2021-04-20 18:05:35 +0530

    Sort a subarray

---
+++ src/com/company/Basic/SortSubarray.java
@@ -0,0 +1,21 @@
+package com.company.Basic;
+
+import java.util.Arrays;
+
+public class SortSubarray {
+    public static void main(String[] args)
+    {
+        // Our arr contains 8 elements
+        int[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
+
+        // Sort subarray from index 1 to 4, i.e.,
+        // only sort subarray {7, 6, 45, 21} and
+        // keep other elements as it is.
+        Arrays.sort(arr, 1, 5);
+
+        System.out.printf("Modified arr[] : %s",
+                Arrays.toString(arr));
+    }
+}
+// A sample Java program to sort a subarray
+// using Arrays.sort().
